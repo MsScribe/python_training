a1b2282e840b0015eb7c301a5904360b3e3039e3
Scherbakova Natalya <19natasha94@bk.ru>
2020-06-12 21:44:07 +0300

    Улучшение читаемоести тестов

--- test_add_group.py
+++ test_add_group.py
@@ -12,20 +12,15 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
         self.create_group(wd, Group(name="privet", header="privet", footer="privet"))
-        self.return_to_groups(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups(wd)
         self.logout(wd)
 
     def logout(self, wd):
@@ -35,6 +30,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
 
     def create_group(self, wd, group):
+        self.open_home_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group firm
@@ -43,11 +39,13 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups(wd)
 
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
