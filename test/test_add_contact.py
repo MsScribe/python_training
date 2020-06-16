fa21a714cf5234cd6e926b8f5d357c120409d92e
Scherbakova Natalya <19natasha94@bk.ru>
2020-06-16 22:21:14 +0300

    Выделена общая фикстура + запуск тестов в одной сессии браузера + создан тестна удаление группы

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,16 +1,7 @@
 # -*- coding: utf-8 -*-
-import pytest
-from fixture.application import Application
 from model.contact import ContactMainInfo
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
-    
 def test_add_contact(app):
     app.open_home_page()
     app.session.login(username="admin", password="secret")
