f0b190fb87b697f638104afc33e64239e8bdef3e
Scherbakova Natalya <19natasha94@bk.ru>
2020-07-05 15:58:10 +0300

    Параметризованы тесты для создания групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,10 +1,33 @@
 # -*- coding: utf-8 -*-
 from model.contact import ContactMainInfo
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_dijits(prefix, maxlen):
+    symbols = string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_email(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) + "@mail.ru"
+
+
+testdata = [ContactMainInfo(firstname=random_string("firstname", 15), middlename=random_string("middlename", 15), lastname=random_string("lastname", 15), nickname=random_string("nickname", 10), title=random_string("title", 20), company=random_string("company", 25), homeaddress=random_string("homeaddress", 20), homephone=random_dijits("+", 11), mobilephone=random_dijits("+", 11), workphone=random_dijits("+", 15), faxphone=random_dijits("+", 7), email=random_email("email", 5), email2=random_email("email", 5), email3=random_email("email", 5), homepage=random_string("https://", 15), bday="1", bmonth="July", byear="1990", aday="6", amonth="November", ayear="1987", address2=random_string("address2", 25), phone2=random_dijits("+", 11), notes=random_string("notes", 30))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = ContactMainInfo(firstname="FirstNameTest", middlename="MiddleNameTest", lastname="LastNameTest", nickname="NickNameTest", title="TestTitle", company="TestCompany", homeaddress="Test Street Test home", homephone="9998887766", mobilephone="+79876543210", workphone="+567", faxphone="3456", email="123qwert@test.ru", email2="1234qwsa@test.com", email3="test123@test.ru", homepage="hhtps://test.ru", bday="1", bmonth="July", byear="1990", aday="6", amonth="November", ayear="1987", address2="Street address", phone2="testhome", notes="blablabla")
     app.open_home_page()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
