b3add91461844b1aaefa7db6efe917001b99fa94
Scherbakova Natalya <19natasha94@bk.ru>
2020-07-17 22:48:44 +0300

    Проверка данных через БД

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,14 +1,14 @@
 from model.contact import ContactMainInfo
-from random import randrange
+import random
 
 
-def test_delete_some_contact(app):
-    if app.contact.count() == 0:
+def test_delete_some_contact(app, db):
+    if len(db.get_contact_list()) == 0:
         app.contact.create(ContactMainInfo(firstname="TestFirstName"))
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
-    app.contact.delete_contact_by_index(index)
-    assert len(old_contacts) - 1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
-    old_contacts[index:index+1] = []
+    old_contacts = db.get_contact_list()
+    contact = random.choice(old_contacts)
+    app.contact.delete_contact_by_id(contact.id)
+    new_contacts = db.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts.remove(contact)
     assert sorted(old_contacts, key=ContactMainInfo.id_or_max) == sorted(new_contacts, key=ContactMainInfo.id_or_max)
